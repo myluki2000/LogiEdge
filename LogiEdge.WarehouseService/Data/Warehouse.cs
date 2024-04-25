1e90cf2daf68d0280c72f3b49f1b9b6b6ef76d41
myluki2000 <lukas636@live.com>
2024-04-25 22:17:17 +0200

    Data.Warehouse: Move Item lists into Warehouse class

--- LogiEdge.WarehouseService/Data/Warehouse.cs
+++ LogiEdge.WarehouseService/Data/Warehouse.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LogiEdge.CustomerService.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace LogiEdge.WarehouseService.Data
 {
@@ -18,6 +19,9 @@ namespace LogiEdge.WarehouseService.Data
 
         public required List<string> AdditionalProperties { get; set; }
         public required List<string> AdditionalPropertiesTypes { get; set; }
+
+        public required List<Item> CurrentItems { get; init; }
+        public required List<Item> HistoricItems { get; init; }
 
         public IEnumerable<(string propertyName, string typeName)> GetAdditionalPropertiesWithTypes()
         {
--- LogiEdge.WarehouseService/Persistence/WarehouseDbContext.cs
+++ LogiEdge.WarehouseService/Persistence/WarehouseDbContext.cs
@@ -12,9 +12,6 @@ namespace LogiEdge.WarehouseService.Persistence
     public class WarehouseDbContext : DbContext
     {
         public required DbSet<Warehouse> Warehouses { get; init; }
-        public required DbSet<Item> CurrentItems { get; init; }
-        public required DbSet<Item> HistoricItems { get; init; }
-        public required DbSet<ItemState> ItemStates { get; init; }
 
         public WarehouseDbContext(DbContextOptions<WarehouseDbContext> options) : base(options) { }
 
