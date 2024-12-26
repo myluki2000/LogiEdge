860adc03cd3cdd69c67fade5ae4398c237039083
myluki2000 <lukas636@live.com>
2024-12-26 16:43:50 +0100

    ItemsPage: Date picker for point in time to view warehouse at

---
+++ LogiEdge.Shared/Utility/DateTimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogiEdge.Shared.Utility
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime SpecifyKind(this DateTime dt, DateTimeKind kind)
+        {
+            return DateTime.SpecifyKind(dt, kind);
+        }
+    }
+}
