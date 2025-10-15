e208546ee899e416c3a36cc423dff9392af6dce5
myluki2000 <lukas636@live.com>
2025-10-15 21:29:29 +0200

    Inbound Transaction Page: auto-saving

--- LogiEdge.CustomerService/Services/CustomerManagementService.cs
+++ LogiEdge.CustomerService/Services/CustomerManagementService.cs
@@ -12,31 +12,51 @@ namespace LogiEdge.CustomerService.Services
 {
     public class CustomerManagementService(IDbContextFactory<CustomerDbContext> dbContextFactory)
     {
-        public IEnumerable<Customer> GetCustomers()
+        public List<Customer> GetCustomers()
         {
-            CustomerDbContext dbContext = dbContextFactory.CreateDbContext();
+            using CustomerDbContext dbContext = dbContextFactory.CreateDbContext();
             
             return dbContext.Customers.ToList();
+        }
+
+        public async Task<List<Customer>> GetCustomersAsync()
+        {
+            await using CustomerDbContext dbContext = await dbContextFactory.CreateDbContextAsync();
+            return await dbContext.Customers.ToListAsync();
         }
 
         public Customer? GetCustomerByAbbreviation(string abbreviation)
         {
             abbreviation = abbreviation.ToUpper();
 
-            CustomerDbContext context = dbContextFactory.CreateDbContext();
+            using CustomerDbContext dbContext = dbContextFactory.CreateDbContext();
 
-            return context.Customers.FirstOrDefault(x => x.Abbreviation == abbreviation);
+            return dbContext.Customers.FirstOrDefault(x => x.Abbreviation == abbreviation);
+        }
+
+        public async Task<Customer?> GetCustomerByAbbreviationAsync(string abbreviation)
+        {
+            abbreviation = abbreviation.ToUpper();
+            await using CustomerDbContext dbContext = await dbContextFactory.CreateDbContextAsync();
+            return await dbContext.Customers.FirstOrDefaultAsync(x => x.Abbreviation == abbreviation);
         }
 
         public Customer CreateCustomer(Customer customer)
         {
-            CustomerDbContext dbContext = dbContextFactory.CreateDbContext();
+            using CustomerDbContext dbContext = dbContextFactory.CreateDbContext();
 
             EntityEntry<Customer> entity = dbContext.Customers.Add(customer);
 
             dbContext.SaveChanges();
 
             return entity.Entity;
+        }
+
+        public async Task<Customer> CreateCustomerAsync(Customer customer)
+        {
+            await using CustomerDbContext dbContext = await dbContextFactory.CreateDbContextAsync();
+            EntityEntry<Customer> entity = dbContext.Customers.Add(customer);
+            return await dbContext.SaveChangesAsync().ContinueWith(t => entity.Entity);
         }
     }
 }
